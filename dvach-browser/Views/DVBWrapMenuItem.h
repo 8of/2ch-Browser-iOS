f308b52a89c31afcbde87153a1a0a0dd4254d2f8
Mini Andy <8ofproject@gmail.com>
2015-03-20 15:30:52 +0300

    Markup in CreatePost View Controller
    Users can call markup menu via selecting text and tapping left-right
    arrows.

---
+++ dvach-browser/Views/DVBWrapMenuItem.h
@@ -0,0 +1,16 @@
+//
+//  DVBWrapBoldMenuItem.h
+//  dvach-browser
+//
+//  Created by Andy on 20/03/15.
+//  Copyright (c) 2015 8of. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVBWrapMenuItem : UIMenuItem
+
+@property (nonatomic, assign) NSUInteger selectedLocation;
+@property (nonatomic, assign) NSUInteger selectedRange;
+
+@end
