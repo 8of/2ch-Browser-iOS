7b6a010f7ed88310401ba1457b774edeb0aef227
Andrey Konstantinov <8ofproject@gmail.com>
2015-10-08 07:13:44 +0300

    New domain HK

--- dvach-browserTests/DVBUrlNinjaTest.m
+++ dvach-browserTests/DVBUrlNinjaTest.m
@@ -29,21 +29,21 @@
 
 - (void)testUrlTypeShouldBeBoardLink
 {
-    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.pm/mobi/"];
+    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.hk/mobi/"];
     UrlNinja *url = [[UrlNinja alloc] initWithUrl:urlToTest];
     XCTAssertEqual(url.type, boardLink, @"Should be external link");
 }
 
 - (void)testUrlTypeShouldBeBoardThreadLink
 {
-    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.pm/mobi/res/531218.html"];
+    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.hk/mobi/res/531218.html"];
     UrlNinja *url = [[UrlNinja alloc] initWithUrl:urlToTest];
     XCTAssertEqual(url.type, boardThreadLink, @"Should be external link");
 }
 
 - (void)testUrlTypeShouldBeBoardThreadPostLink
 {
-    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.pm/mobi/res/472567.html#532581"];
+    NSURL *urlToTest = [[NSURL alloc] initWithString:@"https://2ch.hk/mobi/res/472567.html#532581"];
     UrlNinja *url = [[UrlNinja alloc] initWithUrl:urlToTest];
     XCTAssertEqual(url.type, boardThreadPostLink, @"Should be external link");
 }
