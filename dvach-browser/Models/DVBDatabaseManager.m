04abbf870dc6675f0eca5511b689d1b510522e4f
Andrey Konstantinov <8ofproject@gmail.com>
2017-03-12 18:44:34 +0300

    Migrate to class properties in headers

--- dvach-browser/Models/DVBDatabaseManager.m
+++ dvach-browser/Models/DVBDatabaseManager.m
@@ -12,37 +12,51 @@ static NSString * const DB_FILE = @"dvachDB.sqlite";
 
 @implementation DVBDatabaseManager
 
+#pragma mark - Collections IDs
+
++ (NSString *)dbCollectionThreads
+{
+  return @"kDbCollectionThreads";
+}
+
++ (NSString *)dbCollectionThreadPositions
+{
+  return @"kDbCollectionThreadPositions";
+}
+
+#pragma mark - Construct DB
+
 + (id)sharedDatabase
 {
-    static DVBDatabaseManager *sharedMyManager = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedMyManager = [[self alloc] init];
-
-    });
-    return sharedMyManager;
+  static DVBDatabaseManager *sharedMyManager = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    sharedMyManager = [[self alloc] init];
+  });
+  return sharedMyManager;
 }
 
 - (id)init
 {
-    if (self = [super init]) {
-        if (!_database) {
-            _database = [[YapDatabase alloc] initWithPath:[self constructFullDbPath]];
-        }
+  if (self = [super init]) {
+    if (!_database) {
+      _database = [[YapDatabase alloc] initWithPath:[self constructFullDbPath]];
     }
-    return self;
+  }
+  return self;
 }
 
 - (NSString *)constructFullDbPath
 {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *baseDir = ([paths count] > 0) ? [paths objectAtIndex:0] : NSTemporaryDirectory();
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+  NSString *baseDir = ([paths count] > 0) ? [paths objectAtIndex:0] : NSTemporaryDirectory();
 
-    NSString *databaseName = DB_FILE;
-    NSString *databasePath = [baseDir stringByAppendingPathComponent:databaseName];
-
-    return databasePath;
+  NSString *databaseName = DB_FILE;
+  NSString *databasePath = [baseDir stringByAppendingPathComponent:databaseName];
+  return databasePath;
 }
+
+#pragma mark - Change DB
 
 - (void)clearAll
 {
